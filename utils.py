9729b385545d6be913c32eefc7730a694cbfedb5
Oleksandra Zhyhalo <alya@Mac.lan>
2026-03-15 18:53:50 +0200

    NSDgit add .git add .git add .git add .git add .git add .git add .

--- utils.py
+++ utils.py
@@ -5,3 +5,12 @@ def factorial(n):
     for i in range(2, n + 1):
         result *= i
     return result
+def is_prime(n):
+    Перевіряє, чи є число простим.
+    if n <= 1:
+        return False
+    # Перевіряємо дільники від 2 до кореня з n
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
